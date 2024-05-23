d408b87cc84485c5fcfe84877fbf22ac06aae8a9
simon1655 <simon.izquierdo@urosario.edu.co>
2024-05-22 22:38:58 -0500

    falta poco

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import funciones
 
-categoria_1 = funciones.buscar_por_categoria("Science & Technology", 100)
-status, error = funciones.create_book_database(categoria_1)
-if status:
-    print("Libros guardados correctamente.")
-else:
-    print("Los libros no se pudieron guardar: ", error)
+# Buscar libros por categorías y guardarlos en la base de datos
+categorias = ["Science & Technology", "Science Fiction & Fantasy", "Mystery & Suspense"]
 
-categoria_2 = funciones.buscar_por_categoria("Science Fiction & Fantasy", 100)
-status, error = funciones.create_book_database(categoria_2)
-if status:
-    print("Libros guardados correctamente.")
-else:
-    print("Los libros no se pudieron guardar: ", error)
+for categoria in categorias:
+    libros = funciones.buscar_por_categoria(categoria, 100)
+    status, error = funciones.create_book_database(libros)
+    if status:
+        print(f"Libros de la categoría '{categoria}' guardados correctamente.")
+    else:
+        print(f"Los libros de la categoría '{categoria}' no se pudieron guardar: {error}")
 
-categoria_3 = funciones.buscar_por_categoria("Mystery & Suspense", 100)
-status, error = funciones.create_book_database(categoria_3)
-if status:
-    print("Libros guardados correctamente.")
-else:
-    print("Los libros no se pudieron guardar: ", error)
+
+
 
 
 
